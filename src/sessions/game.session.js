639d18bd7fedddac7c54fd6e9026033c331653b4
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-11-01 07:00:56 +0900

    update: "유저를 게임에 참가시키는 기능 구현"

--- src/sessions/game.session.js
+++ src/sessions/game.session.js
@@ -1,5 +1,24 @@
+import CustomError from '../utils/error/customError.js';
+import { ErrorCodes } from '../utils/error/errorCodes.js';
 import { gameSession } from './sessions';
+import { getuserById } from './user.session.js';
 
 export const getGameSession = () => {
   return gameSession;
+};
+
+export const joinGame = (userId) => {
+  if (!gameSession) {
+    throw new CustomError(ErrorCodes.GAME_NOT_FOUND, '게임 세션을 찾을 수 없습니다.');
+  }
+
+  const user = getuserById(userId);
+  if (!user) {
+    throw new CustomError(ErrorCodes.USER_NOT_FOUND, '유저를 찾을 수 없습니다.');
+  }
+
+  const userExists = gameSession.getUser(userId);
+  if (!userExists) {
+    gameSession.addUser(user);
+  }
 };
