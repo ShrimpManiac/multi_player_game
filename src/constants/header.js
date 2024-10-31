1156817950ff1f1d2375b22c344d534bd0e18751
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-10-31 16:09:42 +0900

    update: "패킷타입 상수 추가"

--- src/constants/header.js
+++ src/constants/header.js
@@ -7,3 +7,10 @@ export const TOTAL_LENGTH_BYTES = 4; // 수정시 readUInt와 writeUInt 메소�
  * 헤더의 패킷타입 부분 크기 (바이트)
  */
 export const PACKET_TYPE_BYTES = 1; // 수정시 readUInt와 writeUInt 메소드도 수정필요
+
+export const PACKET_TYPE = Object.freeze({
+  PING: 0,
+  NORMAL: 1,
+  LOCATION: 2,
+  // 추가시 해당 enum을 사용하는 switch문에 case 추가 필요
+});
