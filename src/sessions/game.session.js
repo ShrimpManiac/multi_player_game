ffd086269ff8a7994249d9586e508efbe2679ca2
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-11-01 07:24:53 +0900

    fix: "leaveGame이 userId 대신 socket을 인자로 받도록 변경"

--- src/sessions/game.session.js
+++ src/sessions/game.session.js
@@ -1,7 +1,7 @@
 import CustomError from '../utils/error/customError.js';
 import { ErrorCodes } from '../utils/error/errorCodes.js';
 import { gameSession } from './sessions';
-import { getUserById } from './user.session.js';
+import { getUserById, getUserBySocket } from './user.session.js';
 
 export const getGameSession = () => {
   return gameSession;
@@ -23,18 +23,18 @@ export const joinGame = (userId) => {
   }
 };
 
-export const leaveGame = (userId) => {
+export const leaveGame = (socket) => {
   if (!gameSession) {
     throw new CustomError(ErrorCodes.GAME_NOT_FOUND, '게임 세션을 찾을 수 없습니다.');
   }
 
-  const user = getUserById(userId);
+  const user = getUserBySocket(socket);
   if (!user) {
     throw new CustomError(ErrorCodes.USER_NOT_FOUND, '유저를 찾을 수 없습니다.');
   }
 
-  const userExists = gameSession.getUser(userId);
+  const userExists = gameSession.getUser(user.id);
   if (!userExists) {
-    gameSession.removePlayer(userId);
+    gameSession.removePlayer(user.id);
   }
 };
