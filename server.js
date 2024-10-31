93e7b8a39be8b20f47c506fe3014d4b75f82f32c
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-10-31 15:49:45 +0900

    refactor: "소켓 이벤트를 파일로 분리"

--- server.js
+++ server.js
@@ -1,23 +1,10 @@
 import net from 'net';
 import { config } from './src/config/config.js';
+import { onConnection } from './src/events/onConnection.js';
 
 const { host, port } = config.server;
 
-const server = net.createServer((socket) => {
-  console.log(`클라이언트가 연결되었습니다: ${socket.remoteAddress}:${socket.remotePort}`);
-
-  socket.on(`data`, (data) => {
-    console.log(`data: ${data}`);
-  });
-
-  socket.on(`end`, () => {
-    console.log(`클라이언트 연결이 종료되었습니다: ${socket.remoteAddress}:${socket.remotePort}`);
-  });
-
-  socket.on(`error`, (error) => {
-    console.error('소켓 에러:', error);
-  });
-});
+const server = net.createServer(onConnection);
 
 server.listen(port, () => {
   console.log(`서버가 ${host}:${port}}에서 실행중입니다.`);
