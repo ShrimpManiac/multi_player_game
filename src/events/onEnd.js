e9a41155318c7f0408b0024bca704e49aa678b4d
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-11-01 07:28:02 +0900

    update: "유저 연결 종료시 자동으로 게임에서 나가도록 설정"

--- src/events/onEnd.js
+++ src/events/onEnd.js
@@ -1,5 +1,7 @@
+import { leaveGame } from '../sessions/game.session.js';
 import { removeUser } from '../sessions/user.session.js';
 export const onEnd = (socket) => () => {
+  leaveGame(socket);
   removeUser(socket);
 
   console.log(`클라이언트 연결이 종료되었습니다: ${socket.remoteAddress}:${socket.remotePort}`);
--- src/events/onError.js
+++ src/events/onError.js
@@ -2,8 +2,10 @@ import { handleError } from '../utils/error/errorHandler.js';
 import CustomError from '../utils/error/customError.js';
 import { ErrorCodes } from '../utils/error/errorCodes.js';
 import { removeUser } from '../sessions/user.session.js';
+import { leaveGame } from '../sessions/game.session.js';
 
 export const onError = (socket) => (error) => {
+  leaveGame(socket);
   removeUser(socket);
 
   console.error('소켓 오류:', error);
