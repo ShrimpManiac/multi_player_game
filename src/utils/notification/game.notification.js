4cce168edc1863657a47eed8995681a12aaf9fe0
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-11-01 07:52:01 +0900

    add: "유저위치정보를 인자로 받아서 LocationUpdate 패킷을 작성하는 기능 구현"

---
+++ src/utils/notification/game.notification.js
@@ -0,0 +1,26 @@
+import { config } from '../../config/config.js';
+import { PACKET_TYPE } from '../../constants/header.js';
+import { getProtoMessages } from '../../init/loadProtos.js';
+
+const makeNotification = (message, type) => {
+  const packetLength = Buffer.alloc(config.packet.totalLength);
+  packetLength.writeUInt32BE(
+    message.length + config.packet.totalLength + config.packet.typeLength,
+    0,
+  );
+
+  const packetType = Buffer.alloc(config.packet.typeLength);
+  packetType.writeUInt8(type, 0);
+
+  return Buffer.concat([packetLength, packetType, message]);
+};
+
+export const createLocationPacket = (userLocations) => {
+  const protoMessages = getProtoMessages();
+  const LocationUpdate = protoMessages.gameNotification.LocationUpdate;
+
+  const payload = { userLocations };
+  const message = LocationUpdate.create(payload);
+  const locationPacket = LocationUpdate.encode(message).finish();
+  return makeNotification(locationPacket, PACKET_TYPE.LOCATION);
+};
