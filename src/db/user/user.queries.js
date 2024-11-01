d1baef5df9044ea97a0d4bd62a9f2a6f17bdb4d7
ShrimpManiac <73187151+ShrimpManiac@users.noreply.github.com>
2024-11-01 10:56:14 +0900

    bugfix: "오자 수정: users -> user"

--- src/db/user/user.queries.js
+++ src/db/user/user.queries.js
@@ -1,5 +1,5 @@
 export const SQL_QUERIES = {
-  FIND_USER_BY_DEVICE_ID: 'SELECT * FROM users WHERE device_id = ?',
+  FIND_USER_BY_DEVICE_ID: 'SELECT * FROM user WHERE device_id = ?',
   CREATE_USER: 'INSERT INTO user (device_id, x, y) VALUES (?, 0, 0)',
   UPDATE_USER_LOGIN: 'UPDATE user SET last_login = CURRENT_TIMESTAMP WHERE device_id = ?',
   UPDATE_USER_LOCATION: `UPDATE user SET x = ?, y = ? WHERE device_id = ?`,
